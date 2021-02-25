1f57123bdb5e5c98b755eba7ef3415c14c8ad162
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-25 19:06:58 +0200

    Book context created and cofigured

---
+++ BookLibrary/BookLibrary/Contexts/BookContext.cs
@@ -0,0 +1,23 @@
+﻿using BookLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Contexts
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public BookContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Books.db");
+        }
+    }
+}
