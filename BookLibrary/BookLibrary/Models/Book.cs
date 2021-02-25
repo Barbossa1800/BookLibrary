fa59b17bf3cd8991433146f09faf36459d1bc5b1
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-02-25 18:55:23 +0200

    Created book model

--- BookLibrary/BookLibrary/Models/Book.cs
+++ BookLibrary/BookLibrary/Models/Book.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace BookLibrary.Models
 {
     public class Book
     {
-
+        public int Id { get; set; }
+        public string Author{ get; set; }
+        public double Price{ get; set; }
+        public DateTime PublishedDate  { get; set; }
+        public string Photo { get; set; }
+        public string Theme { get; set; }
     }
 }
